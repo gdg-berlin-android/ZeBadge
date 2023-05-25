b5dd7cdd52402f14de6fafb02327a782760a717a
Mario Bodemann <mario.bodemann@gmail.com>
2023-05-26 00:55:28 +0200

    improve blinking of echo app

--- zehardware/experiments/code-debug-cdc.py
+++ zehardware/experiments/code-debug-cdc.py
@@ -1,12 +1,11 @@
-#!/bin/python
 # simple cdc dumper
-
 import usb_cdc
 
 if usb_cdc.data:
     import board
     import terminalio
     import digitalio
+    import displayio
     import time
     from adafruit_display_text import label
     
@@ -29,25 +28,28 @@ if usb_cdc.data:
     
     iteration = 0
     while True:
-        if usb_cdc.data.in_waiting > 0:
-            received = usb_cdc.data.readline().decode()
-            if len(received) > 0:
-                print(type(received))
-                print(f"last received: '{received}'")
-                text=f"received: \"{received}\"."
-                text_area = label.Label(terminalio.FONT, text=text)
-                text_area.x = 10
-                text_area.y = 10
-                board.DISPLAY.show(text_area)
-            
-                try:
-                    board.DISPLAY.refresh()
-                except Exception as e:
-                    print(f"nope: {e}")
+        received = usb_cdc.data.readline().decode()
+        if len(received) > 0:
+            print(f"last received: '{received[:23]}'")
+            text=f"received \"{received[:23]}\" ({len(received)})."
+            text_area = label.Label(terminalio.FONT, text=text)
+            text_area.x = 10
+            text_area.y = 10
+            board.DISPLAY.show(text_area)
+        
+            try:
+                board.DISPLAY.refresh()
+            except Exception as e:
+                print(f"nope: {e}")
         
         iteration += 1
         act.value = not act.value
 
 else:
     print("Not Connected to cdc")
+
+
+
+
+
 
