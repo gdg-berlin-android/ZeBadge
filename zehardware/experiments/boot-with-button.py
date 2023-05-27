7277af3b2a303452d5af7a1c58b8b4ce183f143b
Mario Bodemann <mario.bodemann@gmail.com>
2023-05-27 23:23:28 +0200

    rework push button to write boot.py

--- zehardware/experiments/boot-with-button.py
+++ zehardware/experiments/boot-with-button.py
@@ -1,16 +1,17 @@
+# check if we want to program or badge?
+import digitalio
+import board
+
+button = digitalio.DigitalInOut(board.SW_A)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN
+
 import usb_cdc
 import storage
 
 # enable cdc serial for communication
 usb_cdc.enable(console=True, data=True)
 
-# check if we want to program or badge?
+storage.remount("/", readonly=button.value)
 
-from digitalio import DigitalInOut, Direction
-derp = digitalio.DigitalInOut(board.SW_A)
-derp.direction = digitalio.Direction.INPUT
-derp.pull = digitalio.Pull.DOWN
-
-storage.remount("/", readonly=derp.value)
-
-print(f"booted as readonly={derp.value}")
+print(f"booted as readonly={button.value}")
