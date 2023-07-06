a757e66665f3488d95dcbff0bd0e2ef8edb78d3a
ZeBot-220651000 <bot-220651000@berlindroid.de>
2023-07-06 12:02:58 +0200

    add droidcon colors

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeColor.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeColor.kt
@@ -2,10 +2,9 @@ package de.berlindroid.zeapp.zeui.zetheme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DroidConPrimary = Color(0xFF0014E5)
+val DroidConPrimaryVariant = Color(0xFF4D5BED)
+val DroidConSecondary = Color(0xFF3FE4C4)
+val DroidConSecondaryVariant = Color(0xFF9FF2E2)
+val DroidConError = Color(0xFFFA6E50)
+val DroidConNeutral = Color(0xFF20201E)
