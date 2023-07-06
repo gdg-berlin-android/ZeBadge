32a9ea4f370c15e520f0baf39d462dbc04fb42b1
Markus Glodek <markus.glodek@maibornwolff.de>
2023-07-07 01:15:56 +0200

    Remove trailing newlines in ZeVcardStringBuilder

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zebits/ZeVcardStringBuilder.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zebits/ZeVcardStringBuilder.kt
@@ -11,11 +11,11 @@ internal data class ZeVcardStringBuilder(
     fun buildString(): String = listOfNotNull(
         "BEGIN:VCARD",
         "VERSION:2.1",
-        if (formattedName.isNullOrEmpty()) null else "FN:$formattedName\n",
-        if (title.isNullOrEmpty()) null else "TITLE:$title\n",
-        if (phone.isNullOrEmpty()) null else "TEL:$phone\n",
-        if (email.isNullOrEmpty()) null else "EMAIL:$email\n",
-        if (url.isNullOrEmpty()) null else "URL:$url\n",
+        if (formattedName.isNullOrEmpty()) null else "FN:$formattedName",
+        if (title.isNullOrEmpty()) null else "TITLE:$title",
+        if (phone.isNullOrEmpty()) null else "TEL:$phone",
+        if (email.isNullOrEmpty()) null else "EMAIL:$email",
+        if (url.isNullOrEmpty()) null else "URL:$url",
         "END:VCARD"
     ).joinToString("\n")
 }
