1f9f072817f54882813bf249e6160047d73cb1d6
Jan Seeger <jan@alphadev.net>
2023-07-06 12:19:15 +0200

    Make list item actions at the bottom accessible (#98)

--- zeapp/app/src/main/java/de/berlindroid/zeapp/ZeMainActivity.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/ZeMainActivity.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
 import de.berlindroid.zeapp.zemodels.ZeConfiguration
@@ -271,8 +272,10 @@ private fun ZePages(
             ZeLazyColumn(
                 state = lazyListState,
                 contentPadding = PaddingValues(
-                    horizontal = Dimen.One,
-                    vertical = Dimen.Half
+                    start = Dimen.One,
+                    end = Dimen.One,
+                    top = Dimen.Half,
+                    bottom = 140.dp
                 )
             ) {
                 items(
