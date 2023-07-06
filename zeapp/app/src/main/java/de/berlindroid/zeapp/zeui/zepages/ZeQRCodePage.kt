2ea4fe0770a864657d2be76f2dfc826178e8c5f0
Patrick Dorn <patrick@pamado.de>
2023-07-06 14:59:50 +0200

    Fix QR code and QR code title rendering (#116)
    Co-authored-by: Patrick Dorn <patrick.dorn.ext@intive.com>
    Co-authored-by: ZeGitBot for ZeBadge <138262328+ZeGitBot@users.noreply.github.com>

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zepages/ZeQRCodePage.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zepages/ZeQRCodePage.kt
@@ -4,22 +4,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import de.berlindroid.zeapp.PAGE_HEIGHT
 import de.berlindroid.zeapp.PAGE_WIDTH
-import de.berlindroid.zeapp.R
 import de.berlindroid.zeapp.zeui.rememberQrBitmapPainter
 
 /**
@@ -43,22 +44,25 @@ fun QRCodePage(
                 height = pageHeight
             ),
     ) {
-        Column(modifier = Modifier.size(pageWidth - pageHeight)) {
+        Column(
+            modifier = Modifier
+                .width(pageWidth - pageHeight)
+                .fillMaxHeight()
+        ) {
             Text(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFF801000)),
+                    .fillMaxSize()
+                    .background(Color(0xFF000000)),
                 fontFamily = FontFamily.Cursive,
-                fontSize = 10.sp,
-                textAlign = TextAlign.Center,
+                fontSize = 12.sp,
                 color = Color.White,
-                maxLines = 1,
+                maxLines = 3,
                 text = title,
             )
         }
 
         val painter =
-            if (url.isEmpty()) painterResource(id = R.drawable.page_telekom)
+            if (url.isEmpty()) remember { ColorPainter(Color.White) }
             else rememberQrBitmapPainter(url)
         Image(
             painter = painter,
