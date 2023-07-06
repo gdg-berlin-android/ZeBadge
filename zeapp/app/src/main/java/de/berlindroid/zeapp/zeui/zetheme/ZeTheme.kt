85e1724869237b1f9bc7f7ebc3bf79b69fb1f541
zebadgegdg <test2@zedbadge>
2023-07-06 15:10:57 +0200

    use droidcon  colors in app

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
@@ -56,11 +56,6 @@ fun ZeBadgeAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
