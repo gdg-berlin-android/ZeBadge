e965522570754b572824c05a987be3ca36c1fe9a
Louis Tsai <louis993546@gmail.com>
2023-07-06 16:47:16 +0200

    Fix stutter when opening keyboard in NameEditorDialog (#134)
    Co-authored-by: ZeBot-220651000 <bot-220651000@berlindroid.de>

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/ZeNameEditorDialog.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/ZeNameEditorDialog.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material3.AlertDialog
@@ -62,6 +63,7 @@ fun NameEditorDialog(
     }
 
     AlertDialog(
+        modifier = Modifier.imePadding(),
         onDismissRequest = dismissed,
         confirmButton = {
             Button(
@@ -85,7 +87,7 @@ fun NameEditorDialog(
             }
         },
         title = { Text(text = "Add your contact details") },
-        properties = DialogProperties(),
+        properties = DialogProperties(decorFitsSystemWindows = false),
         text = {
             Column {
                 BinaryImageEditor(
