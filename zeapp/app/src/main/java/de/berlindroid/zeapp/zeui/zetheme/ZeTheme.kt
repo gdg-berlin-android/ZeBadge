c462608a39d5c49e5979ac81344cbe8bd54a1c6e
ZeGitBot for ZeBadge <138262328+ZeGitBot@users.noreply.github.com>
2023-07-06 12:21:08 +0200

    Fonts and Theme improvements (#99)
    Co-authored-by: ZeBot-220651000 <bot-220651000@berlindroid.de>

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
@@ -10,21 +10,32 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DroidConPrimary,
+    secondary = DroidConSecondary,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    error = DroidConError,
+    onError = Color.White,
+    background = Color.White,
+    onBackground = DroidConNeutral
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = DroidConPrimaryVariant,
+    secondary = DroidConSecondaryVariant,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    error = DroidConError,
+    onError = Color.White,
+    background = DroidConNeutral,
+    onBackground = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
