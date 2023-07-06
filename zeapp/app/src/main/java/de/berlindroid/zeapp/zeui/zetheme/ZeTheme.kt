bb3a4acb971d24020c5ff27bcddfb38ee1eab8c4
ZeBot-220651000 <bot-220651000@berlindroid.de>
2023-07-06 13:58:04 +0200

    Fix. Package

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
@@ -52,7 +52,7 @@ private val LightColorScheme = lightColorScheme(
 fun ZeBadgeAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
