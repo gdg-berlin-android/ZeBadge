c0d7ab1ec65152aa3deeec6f6337536abcdf1a90
samsruti-dash <samsruti.dash@deliveryhero.com>
2023-07-07 13:26:48 +0200

    Fix Empty Slot Crash

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zevm/ZeBadgeViewModel.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zevm/ZeBadgeViewModel.kt
@@ -392,7 +392,11 @@ class ZeBadgeViewModel @Inject constructor(
      */
     fun sendRandomPageToDevice() {
         val slots = _uiState.value.slots
-        sendPageToDevice(slots.keys.random())
+        if (slots.keys.isEmpty()) {
+            badgeFailure("No Slot Keys are available!")
+        } else {
+            sendPageToDevice(slots.keys.random())
+        }
     }
 
     private fun Int.toBitmap(): Bitmap {
