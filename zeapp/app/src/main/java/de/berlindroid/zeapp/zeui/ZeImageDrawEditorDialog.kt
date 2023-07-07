cf310abb413cc8ca27780be6751d7430507c19a2
LethalMaus <descriptiveanimals@gmail.com>
2023-07-07 09:18:53 +0200

    Fix reported firebase issues

--- zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/ZeImageDrawEditorDialog.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/zeui/ZeImageDrawEditorDialog.kt
@@ -93,7 +93,7 @@ fun ZeImageDrawEditorDialog(
                             detectDragGestures(
                                 onDragStart = { offset ->
                                     currentPosition = offset
-                                    motionEvent = ZeMotionEvent.Down
+                                    if (offset != Offset.Unspecified) motionEvent = ZeMotionEvent.Down
                                 },
                                 onDrag = { pointerInputChange: PointerInputChange, offset: Offset ->
                                     currentPosition =
