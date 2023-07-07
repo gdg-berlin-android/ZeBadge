2826cec0755352d43bb2ba672044b641c08d973d
Sudam Bharat Chavan <sudam.chavan@gmail.com>
2023-07-07 12:31:33 +0200

    better about page ui

--- zeapp/app/src/main/java/de/berlindroid/zeapp/ZeMainActivity.kt
+++ zeapp/app/src/main/java/de/berlindroid/zeapp/ZeMainActivity.kt
@@ -1,6 +1,9 @@
 package de.berlindroid.zeapp
 
+import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,12 +12,14 @@ import androidx.activity.viewModels
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyListState
@@ -40,10 +45,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -200,7 +207,7 @@ private fun ZeScreen(vm: ZeBadgeViewModel, modifier: Modifier = Modifier) {
             },
             content = { paddingValues ->
                 if (isShowingAbout) {
-                    ZeAbout(paddingValues, vm)
+                    ZeAbout(paddingValues, vm, LocalContext.current)
                 } else {
                     ZePages(
                         paddingValues = paddingValues,
@@ -217,6 +224,7 @@ private fun ZeScreen(vm: ZeBadgeViewModel, modifier: Modifier = Modifier) {
 private fun ZeAbout(
     paddingValues: PaddingValues,
     vm: ZeBadgeViewModel,
+    context: Context
 ) {
     val lines by vm.lines.collectAsState()
 
@@ -235,13 +243,28 @@ private fun ZeAbout(
             )
             ZeLazyColumn {
                 items(lines) { line ->
-                    ZeText(
-                        text = line,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        modifier = Modifier.padding(8.dp),
-                        style = MaterialTheme.typography.bodyMedium,
-                        fontSize = 18.sp,
-                    )
+                    ZeRow(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        val email = line.substring(line.indexOf('<').plus(1), line.lastIndexOf('>')).trim()
+                        ZeText(
+                            text = line.substring(0, line.indexOf('<')).trim(),
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.padding(8.dp),
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontSize = 18.sp,
+                        )
+                        ZeIcon(
+                            painter = painterResource(id = R.drawable.email),
+                            contentDescription = "Send random page to badge",
+                            Modifier
+                                .size(20.dp, 20.dp)
+                                .clickable {
+                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("mailto:$email"))
+                                    context. startActivity(intent)
+                                },
+                        )
+                    }
                 }
             }
         }
