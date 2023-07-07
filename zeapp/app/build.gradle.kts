b670b2b37199b7680a15b6856df7414744556a33
Mauricio Pessoa <mauricio.pessoa@mimi.io>
2023-07-07 13:22:30 +0200

    Adding pt-rBR to gradle file

--- zeapp/app/build.gradle.kts
+++ zeapp/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-        resourceConfigurations.addAll(listOf("ar-rEG", "de-rDE", "en-rGB", "fr", "hi", "mr", "nl", "tr", "uk", "ur"))
+        resourceConfigurations.addAll(listOf("ar-rEG", "de-rDE", "en-rGB", "fr", "hi", "mr", "nl", "tr", "uk", "ur", "pt-rBR"))
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
