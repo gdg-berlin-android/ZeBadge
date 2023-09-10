4d51b2635ded5f6bfd6ff11c7721b55d51730c6f
Mario Bodemann <mario.bodemann@gmail.com>
2023-09-11 00:21:12 +0200

    Enhance reliability of sending pictures of the wire (JVM)

--- zeapp/badge/src/macMain/kotlin/de/berlindroid/zekompanion/BadgeManager.kt
+++ zeapp/badge/src/macMain/kotlin/de/berlindroid/zekompanion/BadgeManager.kt
@@ -1,46 +1,46 @@
 package de.berlindroid.zekompanion
 
 import com.fazecast.jSerialComm.SerialPort
-import com.fazecast.jSerialComm.SerialPort.TIMEOUT_WRITE_BLOCKING
-import kotlin.RuntimeException
+import java.lang.Thread.sleep
+import kotlin.IllegalStateException
 
 class AppleBadgeManager : BadgeManager {
     override suspend fun sendPayload(payload: BadgePayload): Result<Int> = if (isConnected()) {
         val badgers = getBadger2040s()
-        val bytes = payload.toBadgeCommand().toByteArray()
+        val command = payload.toBadgeCommand()
+        val commandBytes = command.toByteArray(Charsets.UTF_8)
 
         var written = 0
         for (badger in badgers) {
             try {
                 if (!badger.openPort(300)) {
+                    // couldn't open, try next one
                     continue
                 }
+                setupPort(badger)
 
-                badger.setDTR()
-                badger.baudRate = 115200
-                badger.numDataBits = 8
-                badger.numStopBits = 1
-                badger.parity = 0
+                // write as much as you can and then write some more
+                var offset = 0
+                while (offset < commandBytes.size) {
+                    val length = commandBytes.size - offset
+                    offset += badger.writeBytes(commandBytes, length, offset)
+                }
 
-                badger.setComPortTimeouts(TIMEOUT_WRITE_BLOCKING, 2_0000, 2_000)
+                written = offset
 
-                badger.openPort()
-
-                written = badger.writeBytes(bytes, bytes.size)
-                badger.flushIOBuffers()
+                // wait some more time for the buffers to recover
+                sleep(300)
 
                 if (written > 0) {
                     break
                 }
             } catch (e: RuntimeException) {
-                e.printStackTrace()
                 written = -1
                 break
             } finally {
                 if (badger.isOpen) {
                     badger.closePort()
                 }
-
                 badger.clearDTR()
             }
         }
@@ -48,10 +48,32 @@ class AppleBadgeManager : BadgeManager {
         if (written > 0) {
             Result.success(written)
         } else {
-            Result.failure(RuntimeException("Couldn't write to badge"))
+            Result.failure(RuntimeException("Couldn't write to badge."))
         }
     } else {
         Result.failure(RuntimeException("No badge connected."))
+    }
+
+    private fun setupPort(badger: SerialPort) {
+        if (!badger.setDTR()) {
+            throw IllegalStateException("Could not set dtr on $badger.")
+        }
+
+        if (!badger.setBaudRate(115200)) {
+            throw IllegalStateException("Could not set baud rate on $badger.")
+        }
+
+        if (!badger.setNumDataBits(8)) {
+            throw IllegalStateException("Could not data bit on $badger.")
+        }
+
+        if (!badger.setNumStopBits(1)) {
+            throw IllegalStateException("Could not set num stop bits on $badger.")
+        }
+
+        if (!badger.setParity(0)) {
+            throw IllegalStateException("Could not set parity bit on $badger.")
+        }
     }
 
     override fun isConnected(): Boolean {
@@ -59,7 +81,9 @@ class AppleBadgeManager : BadgeManager {
     }
 
     private fun getBadger2040s() = SerialPort.getCommPorts().filter {
-        it.descriptivePortName.contains("Badger")
+        it.descriptivePortName.contains("Badger") &&
+                it.systemPortPath.contains("cu") &&
+                !it.systemPortPath.contains("tty")
     }
 }
 
