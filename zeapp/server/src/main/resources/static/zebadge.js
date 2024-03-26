8f914e78cfdc7276fb6975f2bdf60263fabefbf8
Mario Bodemann <mario.bodemann@telekom.de>
2024-03-26 18:40:41 +0100

    tweak server side webside

--- zeapp/server/src/main/resources/static/zebadge.js
+++ zeapp/server/src/main/resources/static/zebadge.js
@@ -9,9 +9,6 @@ async function do_port(port, data) {
     })
     sleepFor(delay)
 
-    console.log("post open")
-    console.log(port.getInfo())
-
     const writer = port.writable.getWriter()
     sleepFor(delay)
 
@@ -94,8 +91,6 @@ async function send_to_badge() {
                 {filters: [{usbVendorId:usbVendorId}]}
             )
 
-            console.log(data)
-
             const encoder = new TextEncoder()
             do_port(port, encoder.encode(data))
         } else {
@@ -104,7 +99,7 @@ async function send_to_badge() {
     }
 }
 
-function draw_badge(name, name_size, contact, contact_size) {
+function draw_badge(name, name_size, name_color, contact, contact_size, contact_color, background_image, draw_bars) {
     var canvas = document.getElementById("canvas")
     var ctx = canvas.getContext("2d")
     ctx.textAlign = "center"
@@ -113,18 +108,23 @@ function draw_badge(name, name_size, contact, contact_size) {
     ctx.fillStyle = "#FFFFFF"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    ctx.fillStyle = "#FF0000"
-    ctx.fillRect(0, 0, canvas.width, 30)
-    ctx.fillRect(0, canvas.height - 30 , canvas.width, canvas.height)
+    ctx.drawImage(background_image, 0, 0, 296, 128 )
 
-    ctx.fillStyle = "#FFFFFF"
-    ctx.font = `bold 22px Arial`
-    ctx.fillText("My name is", canvas.width / 2, 30 / 2)
+    if (draw_bars) {
+        ctx.fillStyle = "#FF0000"
+        ctx.fillRect(0, 0, canvas.width, 30)
+        ctx.fillRect(0, canvas.height - 30 , canvas.width, canvas.height)
 
+        ctx.fillStyle = contact_color
+        ctx.font = `bold 22px Arial`
+        ctx.fillText("My name is", canvas.width / 2, 30 / 2)
+    }
+
+    ctx.fillStyle = contact_color
     ctx.font = `${contact_size}px Arial`
     ctx.fillText(contact, canvas.width / 2, canvas.height - 30 / 2)
 
-    ctx.fillStyle = "#000000"
+    ctx.fillStyle = name_color
     ctx.font = `${name_size}px Arial`
     ctx.fillText(name, canvas.width / 2, canvas.height/2)
 }
@@ -133,16 +133,38 @@ function badge_updated() {
     draw_badge(
         document.getElementById("name").value,
         document.getElementById("name_size").value,
+        document.getElementById("name_color").value,
         document.getElementById("contact").value,
         document.getElementById("contact_size").value,
+        document.getElementById("contact_color").value,
+        document.getElementById("background_image"),
+        document.getElementById("draw_bars").checked,
     )
 
     preview_badge()
 }
 
+function background_uploaded() {
+    const file = document.getElementById("background_file").files[0];
+
+    if (!file || !file.type.startsWith("image")) {
+        window.alert("No image file selected, please try again.")
+    } else {
+        bg = document.getElementById("background_image"),
+
+        reader = new FileReader()
+        reader.onload = (e) => {
+            bg.src = e.target.result
+            badge_updated();
+        }
+
+        reader.readAsDataURL(file)
+    }
+}
+
 function check_serial() {
     if (!navigator.serial) {
-        const body = document.getElementsByTagName("body")[0]
+        const body = document.getElementById("main")
         body.innerHTML = "<h1>NO WEB SERIAL</h1>\n" +
          "<p>This app only works with those browsers:\n" +
          "<a href='https://developer.mozilla.org/en-US/docs/Web/API/Web_Serial_API#browser_compatibility'>see web serial compatibility.</a>"
