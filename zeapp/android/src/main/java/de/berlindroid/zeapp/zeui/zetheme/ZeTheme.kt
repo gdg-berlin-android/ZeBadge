35c0ecb263f2e5ededa0d2e115e4c7c8104ed2ae
Mario Bodemann <mario.bodemann@gmail.com>
2024-04-02 22:59:43 +0200

    Fixing Some Design Issues
    fixes #232
    fixes #231

--- zeapp/android/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
+++ zeapp/android/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeTheme.kt
@@ -15,46 +15,21 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = DroidConPrimary,
     secondary = DroidConSecondary,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
+    onPrimary = DroidConOnPrimary,
+    onSecondary = DroidConOnSecondary,
     error = DroidConError,
-    onError = Color.White,
-    background = Color.White,
-    onBackground = DroidConNeutral,
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = DroidConPrimaryVariant,
-    secondary = DroidConSecondaryVariant,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    error = DroidConError,
-    onError = Color.White,
-    background = DroidConNeutral,
-    onBackground = Color.White,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onError = DroidConOnError,
+    background = DroidConBackground,
+    onBackground = DroidConOnBackground,
 )
 
 @Composable
 fun ZeBadgeAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = DarkColorScheme
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
