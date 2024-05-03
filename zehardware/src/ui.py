24ca7e9e42c98e77a91362f55a7a2a655ba0028f
Mario Bodemann <mario.bodemann@gmail.com>
2024-05-03 11:59:21 +0200

    Clarifiy reponsabillity of moduels and app

--- zehardware/src/ui.py
+++ zehardware/src/ui.py
@@ -5,10 +5,13 @@ import time
 import vectorio
 import util
 from adafruit_display_text import label
+import zlib
+import circuitpython_base64 as base64
 
 
 def init(os):
-    os.subscribe("UI_SHOW_BITMAP", _show_image_handler)
+    os.subscribe("UI_SHOW_BITMAP", _show_bitmap_handler)
+    os.subscribe("UI_SHOW_FILE", _show_file_handler)
     os.subscribe("UI_SHOW_TERMINAL", _show_terminal_handler)
     os.subscribe("UI_REFRESH", _refresh_handler)
 
@@ -26,16 +29,32 @@ def _refresh_handler(os, message):
     _refresh_display_save()
 
 
-def _show_image_handler(os, message):
+def _show_bitmap_handler(os, message):
     bitmap, palette = message.value
     del message
 
+    _show_bitmap(bitmap, palette)
+
+
+def _show_bitmap(bitmap, palette):
     tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
 
     group = displayio.Group()
     group.append(tile_grid)
     board.DISPLAY.root_group = group
-    board.DISPLAY.refresh()
+    _refresh_display_save()
+
+
+def _show_file_handler(os, filename):
+    if not filename.endswith('.b64'):
+        filename += '.b64'
+
+    with open(filename, "rb") as file:
+        payload = file.read()
+        bitmap, palette = decode_serialized_bitmap(payload)
+        del payload
+
+        _show_bitmap(bitmap, palette)
 
 
 def _show_terminal_handler(os, message):
@@ -52,3 +71,25 @@ def draw_intro():
     # display.root_group = splash
 
     _refresh_display_save()
+
+
+def decode_serialized_bitmap(payload, width=296, height=128):
+    compressed_bytes = base64.b64decode(payload)
+    binarized_bytes = zlib.decompress(compressed_bytes)
+
+    bitmap = displayio.Bitmap(width, height, 2)
+
+    palette = displayio.Palette(2)
+    palette[0] = 0x000000
+    palette[1] = 0xFFFFFF
+
+    for y in range(height):
+        print("B", end="")
+        for x in range(width):
+            # Pretend you understand this part
+            byte_index = (y * (width // 8)) + (x // 8)
+            bit_index = 7 - (x % 8)
+            pixel_value = (binarized_bytes[byte_index] >> bit_index) & 1
+            bitmap[x, y] = pixel_value
+
+    return bitmap, palette
