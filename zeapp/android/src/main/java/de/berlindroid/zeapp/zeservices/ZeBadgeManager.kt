fdb3392c4c2650c71d597f851260caf9936cdef9
Mario Bodemann <mario.bodemann@gmail.com>
2024-05-13 10:37:01 +0200

    Add configuration editor.

--- zeapp/android/src/main/java/de/berlindroid/zeapp/zeservices/ZeBadgeManager.kt
+++ zeapp/android/src/main/java/de/berlindroid/zeapp/zeservices/ZeBadgeManager.kt
@@ -78,5 +78,58 @@ class ZeBadgeManager @Inject constructor(
         }
     }
 
+    /**
+     * Return the current active configuration.
+     */
+    suspend fun listConfiguration(): Result<Map<String, String>> {
+        val payload = BadgePayload(
+            type = "config_list",
+            meta = "",
+            payload = "",
+        )
+
+        if (badgeManager.sendPayload(payload).isSuccess) {
+            val responses = mutableListOf<Result<String>>()
+            while (true) {
+                val response = badgeManager.readResponse()
+                if (response.isSuccess) {
+                    responses += response
+                } else {
+                    break
+                }
+            }
+
+            val successful = responses.filter { it.isSuccess }.joinToString(separator = "\n") {
+                it.getOrDefault("")
+            }
+
+            print("message: '$successful'")
+            val kv = mapOf(
+                *successful.split(",").map {
+                    val (k, v) = it.split(" = ")
+                    k to v
+                }.toTypedArray(),
+            )
+            return Result.success(kv)
+        } else {
+            return Result.failure(NoSuchElementException())
+        }
+    }
+
+    /**
+     * Update configuration on badge..
+     */
+    suspend fun updateConfiguration(configuration: Map<String, String>): Result<Int> {
+        val config = configuration.entries.joinToString(separator = ",")
+
+        val payload = BadgePayload(
+            type = "config_update",
+            meta = "",
+            payload = config,
+        )
+
+        return badgeManager.sendPayload(payload)
+    }
+
     fun isConnected(): Boolean = badgeManager.isConnected()
 }
