d96cd47f3b28acdd5299d0592d545d48aba811e6
Mario Bodemann <mario.bodemann@gmail.com>
2024-05-19 17:13:00 +0200

    fix: typo

--- zehardware/src/util.py
+++ zehardware/src/util.py
@@ -2,7 +2,7 @@ import traceback
 
 
 def exception_to_readable(exception) -> str:
-    """format a given exeption to a readable error message."""
+    """format a given exception to a readable error message."""
     message = str(exception)
     trace = traceback.format_exception(exception)
     result = "Reason: "
