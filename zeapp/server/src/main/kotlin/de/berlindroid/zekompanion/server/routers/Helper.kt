167de50e00ff58e7df81a73979929e2173bbad4e
Mario Bodemann <mario.bodemann@gmail.com>
2024-06-28 21:18:06 +0200

    add ai framework and initial name generation

--- zeapp/server/src/main/kotlin/de/berlindroid/zekompanion/server/routers/Helper.kt
+++ zeapp/server/src/main/kotlin/de/berlindroid/zekompanion/server/routers/Helper.kt
@@ -7,6 +7,8 @@ import io.ktor.server.request.header
 import io.ktor.server.response.respondText
 import io.ktor.util.pipeline.PipelineContext
 
+private const val AUTH_TOKEN_ENV = "ZESERVER_AUTH_TOKEN"
+private const val AUTH_TOKEN_HEADER = "ZeAuth"
 
 suspend fun PipelineContext<Unit, ApplicationCall>.withParameter(
     key: String,
@@ -21,9 +23,15 @@ suspend fun PipelineContext<Unit, ApplicationCall>.withParameter(
 }
 
 suspend fun PipelineContext<Unit, ApplicationCall>.ifAuthorized(block: suspend PipelineContext<Unit, ApplicationCall>.() -> Unit) {
-    val authHeader = call.request.header("ZeAuth")
-    val authEnv = System.getenv("ZESERVER_AUTH_TOKEN")
-    if (authEnv.isEmpty() || authHeader == null || authEnv != authHeader) {
+    val authHeader = call.request.header(AUTH_TOKEN_HEADER)
+    val authEnv = System.getenv(AUTH_TOKEN_ENV)
+
+    if (authEnv.isNullOrBlank()) {
+        println("Auth env ('${AUTH_TOKEN_ENV}') environment var is 'null' or empty, you will not be able to do admin level tasks. " +
+                "Set the env var and restart the server.")
+    }
+
+    if (authEnv.isNullOrBlank() || authHeader == null || authEnv != authHeader) {
         call.respondText(status = HttpStatusCode.Forbidden, text = "Forbidden")
     } else {
         block()
