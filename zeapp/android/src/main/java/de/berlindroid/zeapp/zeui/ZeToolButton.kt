95a0bdd33af86a4d277ae3ff3e9786d4c5f96f72
rockgecko-development <rockgecko-development@users.noreply.github.com>
2024-07-03 23:18:18 +0200

    missing import

--- zeapp/android/src/main/java/de/berlindroid/zeapp/zeui/ZeToolButton.kt
+++ zeapp/android/src/main/java/de/berlindroid/zeapp/zeui/ZeToolButton.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.core.R
