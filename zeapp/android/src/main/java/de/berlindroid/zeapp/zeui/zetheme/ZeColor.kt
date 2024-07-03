40cbb4688da1cdad610a79047b096104d0a62630
Aditya Arora <adityaarora1@gmail.com>
2024-07-03 15:56:31 +0200

    Refactor colors

--- zeapp/android/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeColor.kt
+++ zeapp/android/src/main/java/de/berlindroid/zeapp/zeui/zetheme/ZeColor.kt
@@ -5,3 +5,6 @@ import androidx.compose.ui.graphics.Color
 val ZeWhite = Color(0xFFFFFFFF)
 val ZeGrey = Color(0xFF7E7E7E)
 val ZeBlack = Color(0xFF000000)
+val ZeTeal = Color(0xFF6200EE)
+val ZePurple = Color(0xFF03DAC6)
+val ZeCarmine = Color(0xFFB00020)
