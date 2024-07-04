89fcf82b0c00dc0540ffa29c549d6c2764837cbc
Christophe Beyls <software@digitalia.be>
2024-07-04 14:31:23 +0200

    replace Gson with Kotlinx Serialization and align Kotlinx Serialization plugin version with Kotlin version

--- zeapp/build.gradle.kts
+++ zeapp/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.detekt.gradle) apply false
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.license.report.gradle) apply false
--- zeapp/desktop/build.gradle.kts
+++ zeapp/desktop/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
-    kotlin("plugin.serialization") version "1.9.0"
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 group = "com.example"
