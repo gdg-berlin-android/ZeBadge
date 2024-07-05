b4fc74221287416bbe5dc2d33bc01ae690c3f5b7
Christophe Beyls <software@digitalia.be>
2024-07-05 22:47:03 +0200

    fix: revert changes in ZeBadgeKompanion so the desktop app can compile again

--- zeapp/desktop/src/main/kotlin/de/berlindroid/zekompanion/desktop/ZeBadgeKompanion.kt
+++ zeapp/desktop/src/main/kotlin/de/berlindroid/zekompanion/desktop/ZeBadgeKompanion.kt
@@ -8,6 +8,9 @@ import androidx.compose.ui.awt.ComposePanel
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import de.berlindroid.zekompanion.BADGE_HEIGHT
+import de.berlindroid.zekompanion.BADGE_WIDTH
+import de.berlindroid.zekompanion.BadgePayload
 import de.berlindroid.zekompanion.base64
 import de.berlindroid.zekompanion.buildBadgeManager
 import de.berlindroid.zekompanion.desktop.ui.DrawNameBadge
@@ -17,9 +20,6 @@ import de.berlindroid.zekompanion.desktop.ui.fromJSONtoConfig
 import de.berlindroid.zekompanion.ditherFloydSteinberg
 import de.berlindroid.zekompanion.resize
 import de.berlindroid.zekompanion.toBinary
-import de.berlindroid.zekompanion.BADGE_WIDTH
-import de.berlindroid.zekompanion.BADGE_HEIGHT
-import de.berlindroid.zekompanion.BadgePayload.*
 import de.berlindroid.zekompanion.zipit
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -161,8 +161,10 @@ private fun sendImageToBadge(image: BufferedImage, callback: (Result<Int>) -> Un
         GlobalScope.launch {
             with(buildBadgeManager("")) {
                 if (isConnected()) {
-                    val payload = PreviewPayload(
+                    val payload = BadgePayload(
                         debug = false,
+                        type = "preview",
+                        meta = "",
                         image.toPayload(),
                     )
 
