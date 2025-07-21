330c4a2873f63309d84c279c9a7e0d1d967086e4
Alishen <alisene@gmail.com>
2025-07-21 03:40:35 +0200

    fix(equals): simplify type check for UserInfo equality comparison

--- zeapp/android/src/main/java/de/berlindroid/zeapp/zeservices/ZeBadgeConfigParser.kt
+++ zeapp/android/src/main/java/de/berlindroid/zeapp/zeservices/ZeBadgeConfigParser.kt
@@ -84,9 +84,7 @@ data class UserInfo(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as UserInfo
+        if (other !is UserInfo) return false
 
         if (id != other.id) return false
         if (name != other.name) return false
